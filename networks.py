ed2aa3cebbd3ca9fa1672e20860e617e6e1bb16b
Brandon Qiao <qiaobrandon@gmail.com>
2020-04-22 14:46:59 -0400

    Generator architecture

---
+++ networks.py
@@ -0,0 +1,111 @@
+
+import torch
+import torch.nn as nn
+
+
+class ResnetGenerator(nn.Module):
+    """ Generator class utilizing resnets"""
+
+    def __init__(self, input_channels, output_channels, ngf=64, normalization='instance', num_blocks=9):
+        """
+        Paramters:
+            input_channels: number of channels in input image
+            output_channels: number of channels in output image
+            ngf: number of filters use in convolutions
+            normalization: batch or instance
+            num_blocks: number of resnet blocks. 9 for 256x256, 6 for lower
+        """
+        model = []
+        
+        # Paper states to use instance norm, but we'll give the option of using batch
+        if normalization == 'instance':
+            norm_layer = nn.InstanceNorm2d
+        elif normalization == 'batch':
+            norm_layer = nn.BatchNorm2d
+        else:
+            raise NotImplementedError('ResnetGenerator: Normaliztion [%s] is not implemented' % normalization)
+
+        use_bias = False
+        if normalization == 'instance':
+            use_bias = True
+
+        """ First Convolution Block"""
+        # C7s1-64 block
+        model += [nn.ReflectionPad2d(3), 
+                  nn.Conv2d(input_channels, ngf, kernel_size=7, bias=use_bias), 
+                  norm_layer(ngf), 
+                  nn.ReLU(True)]
+
+        """Downsampling"""
+        # d128 block
+        model += [nn.Conv2d(ngf, 128, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                  norm_layer(128),
+                  nn.ReLU(True)]
+        # d256 block
+        model += [nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=use_bias),
+                  norm_layer(256),
+                  nn.ReLU(True)]
+
+        """Resnet Blocks"""
+        for _ in range(num_blocks):
+            # R256 blocks
+            model+=[ResnetBlock(256, norm_layer, use_bias)]
+        
+        """Upsampling"""
+        # U128 block
+        model += [nn.ConvTranspose2d(256, 128,
+                                kernel_size=3, stride=2,
+                                padding=1, output_padding=1,
+                                bias=use_bias),
+                  norm_layer(128),
+                  nn.ReLU(True)]
+
+        # U64 block
+        model += [nn.ConvTranspose2d(128, 64,
+                                kernel_size=3, stride=2,
+                                padding=1, output_padding=1,
+                                bias=use_bias),
+                  norm_layer(64),
+                  nn.ReLU(True)]
+        
+        """Final Convolution block"""
+        # C7s1-3 block
+        model += [nn.ReflectionPad2d(3), 
+                  nn.Conv2d(64, output_channels, kernel_size=7, bias=use_bias)]
+                  
+        # Last block uses tanh instead of ReLU
+        model += [nn.Tanh()]
+
+        self.model = nn.Sequential(*model)
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class ResnetBlock(nn.Module):
+    def __init__ (self, num_filters, norm_layer, use_bias):
+        super(ResnetBlock, self).__init__()
+        self.conv_block = self.build_conv_block(num_filters, norm_layer, use_bias)
+    
+    def build_conv_block(self, num_filters, norm_layer, use_bias):
+        """Image should come out the same size"""
+        conv_block = []
+        
+        # Paper said reflection padding was used to reduce artifacts
+        conv_block += [nn.ReflectionPad2d(1)]
+
+        # Convolution with kernel size 3, and bias if InstanceNorm is used
+        conv_block += [nn.Conv2d(num_filters, num_filters, kernel_size=3, bias=use_bias), 
+                      norm_layer(num_filters), 
+                      nn.ReLU(True)]
+
+        conv_block += [nn.ReflectionPad2d(1)]
+
+        conv_block += [nn.Conv2d(num_filters, num_filters, kernel_size=3, bias=use_bias), 
+                       norm_layer(num_filters)]
+
+        return nn.Sequential(*conv_block)
+    
+    def forward(self, x):
+        return x + self.conv_block(x)
+
